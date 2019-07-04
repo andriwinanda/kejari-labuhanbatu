c028f9f6f2bf24bdcc7c659761d4669a8815e15d
andri winanda <andriwinanda1@gmail.com>
2019-07-04 16:06:53 +0700

    fix berita layout

--- src/pages/Berita.vue
+++ src/pages/Berita.vue
@@ -1,41 +1,61 @@
 <template>
-  <div class="columns is-multiline">
+  <div class="section columns is-multiline">
     <div class="column is-8">
-      <div class="section">
+      <div class="isi-berita">
         <h1 class="title is-3">Pembukaan Acara Rapat Kerja Kejaksaan RI Tahun 2018</h1>
-        <p>
-          Oleh : Kejati Sumut
-          <span class="is-pulled-right">Diupload : 28/6/2019</span>
-        </p>
-        <hr>
-        <img src="https://strategi.id/wp-content/uploads/2018/11/rakernas1.jpg" alt>
-        <br>
+        <div>
+          <small>Oleh : Kejati Sumut</small>
+          <span class="is-pulled-right">
+            <small>Diupload : 28/6/2019</small>
+          </span>
+        </div>
+        <hr />
+        <img src="https://strategi.id/wp-content/uploads/2018/11/rakernas1.jpg" alt />
+        <br />
         <p class="has-text-justified">
-          Selasa 27 November 2018, Kejaksaan Republik Indonesia menggelar acara Rapat Kerja di Hotel Grand Inna Bali Beach. Rapat Kerja Kejaksaan RI tahun 2018 diawali dengan laporan ketua panitia Raker Kejaksaan RI Tahun 2018 Jaksa Agung Muda Perdata dan Tata Usaha Negara Luke Larasati Agustina, melaporkan sumber dana pelaksaan raker yang berasal dari DIPA Kejaksaan RI Tahun Anggaran 2018, dan menyampaikan rangkaian kegiatan raker yang menghadirkan pembicara dari berbagai elemen seperti dari Komisi Yudisial, Duta Besar serta pemateri lainnya. Rakernas juga sekaligus sebagai evaluasi kinerja Kejaksaan RI di seluruh Indonesia. Acara ini dihadiri oleh Jaksa Agung RI HM Prasetyo, Wakil Jaksa Agung RI Arminsyah, Para Jaksa Agung Muda, Kabandiklat, Para Kajati, Para Asisten, dan Pengurus Ikatan Adhyaksa Dharmakarini seluruh Indonesia serta Peserta Rapat Kerja Kejaksaan RI Tahun 2018.
-          <br>Rapat Kerja Kejaksaan RI Tahun 2018 ini dibuka secara resmi oleh Jaksa Agung RI HM Prasetyo dan ditandai dengan pemukulan gong.
-          <br>Dalam arahannya HM Prasetyo menyambut baik dilaksanakannya acara Rakernas Kejaksaan RI 2018 yang mengusung tema KUKUHKAN SEMANGAT BARU, BANGUN PERSEPSI DAN CITRA DIRI, UKIR PRESTASI UNTUK NEGERI, untuk menyamakan persepsi dalam upaya penegakan hukum di negeri ini
-          <br>Jaksa Agung HM Prasetyo meminta kepada seluruh peserta yang mengikuti Raker agar memiliki komitmen dalam memperbaiki citra diri dan mengukir prestasi, berkontribusi dan berdikasi bagi bangsa dan negara demi reformasi birokrasi dan peningkatan pelayanan Kejaksaan yang terbuka dan transparan.
-          <br>Semoga dengan Rapat Kerja Kejaksaan RI Tahun 2018 ini seluruh peserta semakin bersemangat dalam menjalankan tugas sebagai penegak hukum. Dan bekerja dengan kesungguhan, keuletan, dan berintegritas.
-          <br>Sumber : Kejati Sumut
+          Selasa 27 November 2018, Kejaksaan Republik Indonesia menggelar acara Rapat Kerja di Hotel Grand Inna
+          Bali Beach. Rapat Kerja Kejaksaan RI tahun 2018 diawali dengan laporan ketua panitia Raker Kejaksaan
+          RI Tahun 2018 Jaksa Agung Muda Perdata dan Tata Usaha Negara Luke Larasati Agustina, melaporkan sumber
+          dana pelaksaan raker yang berasal dari DIPA Kejaksaan RI Tahun Anggaran 2018, dan menyampaikan
+          rangkaian kegiatan raker yang menghadirkan pembicara dari berbagai elemen seperti dari Komisi
+          Yudisial, Duta Besar serta pemateri lainnya. Rakernas juga sekaligus sebagai evaluasi kinerja
+          Kejaksaan RI di seluruh Indonesia. Acara ini dihadiri oleh Jaksa Agung RI HM Prasetyo, Wakil Jaksa
+          Agung RI Arminsyah, Para Jaksa Agung Muda, Kabandiklat, Para Kajati, Para Asisten, dan Pengurus Ikatan
+          Adhyaksa Dharmakarini seluruh Indonesia serta Peserta Rapat Kerja Kejaksaan RI Tahun 2018.
         </p>
+        <p class="has-text-justified">
+          Rapat Kerja Kejaksaan RI Tahun 2018 ini dibuka secara resmi oleh Jaksa Agung RI HM Prasetyo dan ditandai
+          dengan pemukulan gong. Dalam arahannya HM Prasetyo menyambut baik dilaksanakannya acara
+          Rakernas Kejaksaan RI 2018 yang mengusung tema KUKUHKAN SEMANGAT BARU, BANGUN PERSEPSI DAN CITRA DIRI,
+          UKIR PRESTASI UNTUK NEGERI, untuk menyamakan persepsi dalam upaya penegakan hukum di negeri ini
+        </p>
+        <p class="has-text-justified">
+          Jaksa Agung HM Prasetyo meminta kepada seluruh peserta yang mengikuti Raker agar memiliki komitmen dalam
+          memperbaiki citra diri dan mengukir prestasi, berkontribusi dan berdikasi bagi bangsa dan negara
+          demi reformasi birokrasi dan peningkatan pelayanan Kejaksaan yang terbuka dan transparan. Semoga dengan
+          Rapat Kerja Kejaksaan RI Tahun 2018 ini seluruh peserta semakin bersemangat dalam
+          menjalankan tugas sebagai penegak hukum. Dan bekerja dengan kesungguhan, keuletan, dan berintegritas.
+        </p>
+        <br />
+        <small>Sumber : Kejati Sumut</small>
       </div>
     </div>
-    <div class="column is-4 section berita-lain" style>
+    <div class="column is-4 berita-lain" style>
       <p class="title is-5">Berita Terpopuler</p>
-      <hr>
+      <hr />
 
       <article class="media">
         <figure class="media-left">
           <p class="image is-64x64">
-            <img src="https://bulma.io/images/placeholders/128x128.png">
+            <img src="https://bulma.io/images/placeholders/128x128.png" />
           </p>
         </figure>
         <div class="media-content">
           <div class="content">
             <p class="has-text-justified">
               <strong>Barbara Middleton</strong>
-              <br>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse pellentesque...
-              <br>
+              <br />Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse pellentesque...
+              <br />
               <small>
                 <a>Selengkapnya »</a>
               </small>
@@ -43,20 +63,19 @@
           </div>
         </div>
       </article>
-      
 
       <article class="media">
         <figure class="media-left">
           <p class="image is-64x64">
-            <img src="https://bulma.io/images/placeholders/128x128.png">
+            <img src="https://bulma.io/images/placeholders/128x128.png" />
           </p>
         </figure>
         <div class="media-content">
           <div class="content">
             <p class="has-text-justified">
               <strong>Barbara Middleton</strong>
-              <br>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse pellentesque...
-              <br>
+              <br />Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse pellentesque...
+              <br />
               <small>
                 <a>Selengkapnya »</a>
               </small>
@@ -64,20 +83,19 @@
           </div>
         </div>
       </article>
-      
 
       <article class="media">
         <figure class="media-left">
           <p class="image is-64x64">
-            <img src="https://bulma.io/images/placeholders/128x128.png">
+            <img src="https://bulma.io/images/placeholders/128x128.png" />
           </p>
         </figure>
         <div class="media-content">
           <div class="content">
             <p class="has-text-justified">
               <strong>Barbara Middleton</strong>
-              <br>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse pellentesque...
-              <br>
+              <br />Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse pellentesque...
+              <br />
               <small>
                 <a>Selengkapnya »</a>
               </small>
@@ -85,20 +103,19 @@
           </div>
         </div>
       </article>
-      
 
       <article class="media">
         <figure class="media-left">
           <p class="image is-64x64">
-            <img src="https://bulma.io/images/placeholders/128x128.png">
+            <img src="https://bulma.io/images/placeholders/128x128.png" />
           </p>
         </figure>
         <div class="media-content">
           <div class="content">
             <p class="has-text-justified">
               <strong>Barbara Middleton</strong>
-              <br>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse pellentesque...
-              <br>
+              <br />Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse pellentesque...
+              <br />
               <small>
                 <a>Selengkapnya »</a>
               </small>
@@ -106,20 +123,19 @@
           </div>
         </div>
       </article>
-      
 
       <article class="media">
         <figure class="media-left">
           <p class="image is-64x64">
-            <img src="https://bulma.io/images/placeholders/128x128.png">
+            <img src="https://bulma.io/images/placeholders/128x128.png" />
           </p>
         </figure>
         <div class="media-content">
           <div class="content">
             <p class="has-text-justified">
               <strong>Barbara Middleton</strong>
-              <br>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse pellentesque...
-              <br>
+              <br />Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse pellentesque...
+              <br />
               <small>
                 <a>Selengkapnya »</a>
               </small>
@@ -127,20 +143,19 @@
           </div>
         </div>
       </article>
-      
 
       <article class="media">
         <figure class="media-left">
           <p class="image is-64x64">
-            <img src="https://bulma.io/images/placeholders/128x128.png">
+            <img src="https://bulma.io/images/placeholders/128x128.png" />
           </p>
         </figure>
         <div class="media-content">
           <div class="content">
             <p class="has-text-justified">
               <strong>Barbara Middleton</strong>
-              <br>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse pellentesque...
-              <br>
+              <br />Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse pellentesque...
+              <br />
               <small>
                 <a>Selengkapnya »</a>
               </small>
@@ -148,22 +163,37 @@
           </div>
         </div>
       </article>
-      
     </div>
   </div>
 </template>
 <style>
+.section{
+  padding-top: 2rem
+}
 .berita-lain {
   border-left: 1px solid gainsboro;
-  margin-top: 3rem;
-  padding-top: 1rem;
-  padding-right: 2.3rem;
+  padding: 1rem;
+  padding-left: 2rem;
 }
+
+.isi-berita p {
+  text-indent: 2rem;
+  padding-top: 1.5rem;
+}
+
 @media only screen and (max-width: 780px) {
   .berita-lain {
-    padding: 2.3rem !important;
+    padding: 0.75rem !important;
+    border-left: none;
   }
 }
+
+@media only screen and (min-width: 780px) {
+  .isi-berita {
+    padding-right: 1rem !important;
+  }
+}
+
 p {
   color: black;
 }
